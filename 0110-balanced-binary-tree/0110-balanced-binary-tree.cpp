ab9b98dae54b75c25e1dcbd1dd6d8ded0263d1d2
Aayushi Goyal <aayushi1754@gmail.com>
2026-07-18 15:48:01 +0530

    Time: 83 ms (5.06%), Space: 23.2 MB (20.99%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,24 +11,30 @@
  */
 class Solution {
 public:
+int height(TreeNode* root)
+{
+    if(root==NULL)
+    return 0;
+    int l=height(root->left);
+    int r=height(root->right);
+    return 1+max(l,r);
+}
     bool isBalanced(TreeNode* root) {
-        // If the tree is empty, we can say it’s balanced...
-        if (root == NULL)  return true;
-        // Height Function will return -1, when it’s an unbalanced tree...
-		if (Height(root) == -1)  return false;
-		return true;
-	}
-    // Create a function to return the “height” of a current subtree using recursion...
-	int Height(TreeNode* root) {
-        // Base case...
-		if (root == NULL)  return 0;
-        // Height of left subtree...
-		int leftHeight = Height(root->left);
-        // Height of height subtree...
-		int rightHight = Height(root->right);
-        // In case of left subtree or right subtree unbalanced or their heights differ by more than ‘1’, return -1...
-		if (leftHeight == -1 || rightHight == -1 || abs(leftHeight - rightHight) > 1)  return -1;
-        // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
-		return max(leftHeight, rightHight) + 1;
+        if(root==NULL)
+        return true;
+        int l=height(root->left);
+        bool leftbalance=isBalanced(root->left);
+        if(leftbalance==false)
+        return false;
+        int r=height(root->right);
+        bool rightbalance=isBalanced(root->right);
+        if(rightbalance==false)
+        return false;
+
+        if(abs(l-r)<=1)
+        return true;
+        else
+        return false;
+        
     }
 };
